63a69be0ba58c4c6aeb3a60a52ef66e76a9fb6dc
Alexandre Furlan <alexandrepfurlan@gmail.com>
2025-09-03 23:32:15 -0300

    upload sql codes (2)

--- sql/date_ranges.sql
+++ sql/date_ranges.sql
@@ -1,31 +1,32 @@
 
-WITH daily_metrics AS (
-    SELECT 		
-        date,
-        date(date - INTERVAL '30 days') as window_prev_date, 
-        AVG(spend/conversions) as avg_CAC,
-        AVG(conversions*100/spend) as avg_ROAS,
-        SUM(spend/conversions) as sum_CAC,
-        SUM(conversions*100/spend) as sum_ROAS
-    FROM postgres.public.ads_spend
-    GROUP BY date
+CREATE OR REPLACE FUNCTION metrics_in_range_date(
+    p_start_date DATE,
+    p_end_date DATE
 )
-SELECT 
-    date,
-    window_prev_date,
-    avg_cac,
-    LAG(avg_cac, 30) OVER (ORDER BY date) as avg_cac_window_prev_date,
-    avg_roas,
-    LAG(avg_roas, 30) OVER (ORDER BY date) as avg_roas_window_prev_date,
-    avg_cac - LAG(avg_cac, 30) OVER (ORDER BY date) as avg_cac_delta,
-    avg_roas - LAG(avg_roas, 30) OVER (ORDER BY date) as avg_roas_delta,
-    -- 
-    sum_cac,
-    LAG(sum_cac, 30) OVER (ORDER BY date) as sum_cac_window_prev_date,
-    sum_roas,
-    LAG(sum_roas, 30) OVER (ORDER BY date) as sum_roas_window_prev_date,
-    sum_cac - LAG(sum_cac, 30) OVER (ORDER BY date) as sum_cac_delta,
-    sum_roas - LAG(sum_roas, 30) OVER (ORDER BY date) as sum_roas_delta
-FROM daily_metrics
-where date >= date('2025-03-02')
-ORDER BY date desc;
+RETURNS TABLE (
+    start_date DATE,
+    end_date DATE,
+    avg_cac NUMERIC,
+    avg_roas numeric,
+    sum_cac NUMERIC,
+    sum_roas numeric
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        date,
+		date,
+        AVG(a.spend/a.conversions) as avg_CAC,
+        AVG(a.conversions*100/a.spend) as avg_ROAS,
+        SUM(a.spend/a.conversions) as sum_CAC,
+        SUM(a.conversions*100/a.spend) as sum_ROAS
+    FROM postgres.public.ads_spend a
+    WHERE a.date BETWEEN p_start_date AND p_end_date
+	group by a.date
+    ORDER BY a.date;
+END;
+$$;
+
+select * from  metrics_in_range_date('2025-03-22','2025-06-12')
